08402636d997a7ad155c8c3e17e87d3cc7b01370
Igor MARTY <igor.marty@telecomnancy.net>
2020-03-17 15:04:14 +0100

    nav css + data

---
+++ src/app/services/fakeData.ts
@@ -0,0 +1,42 @@
+export const BooksArray = [
+  {
+    name: "I am a book",
+    author: "Igor MARTY",
+    available: true,
+    category: "biography",
+    price: 1,
+    publishedDate: '16/02/2020',
+    ageLimite: 18,
+    isbn: 9782072705242
+  },
+  {
+    name: "I am a different book",
+    author: "Igor MARTY but a different one",
+    available: true,
+    category: "biography",
+    price: 2,
+    publishedDate: '21/02/2020',
+    ageLimite: 19,
+    isbn: 9782072705243
+  },
+  {
+    name: "Win Billion at Home during Crysis",
+    author: "Jean Michel Arnaque",
+    available: false,
+    category: "biography",
+    price: 500,
+    publishedDate: '06/03/2020',
+    ageLimite: 0,
+    isbn: 9782072705244
+  },
+  {
+    name: "Learn French with my wife",
+    author: "Manu Mac On",
+    available: false,
+    category: "educational",
+    price: 10,
+    publishedDate: '10/03/2020',
+    ageLimite: 21,
+    isbn: 9782072705245
+  },
+]
