ec75112222ccea9cfdf1d4030e614a2814443178
Igor MARTY <igor.marty@telecomnancy.net>
2020-03-18 12:16:23 +0100

    almost working numbers

--- src/app/components/kpi-card/kpi-card.component.ts
+++ src/app/components/kpi-card/kpi-card.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class KpiCardComponent implements OnInit {
   @Input() title;
   @Input() color;
+  @Input() number;
 
   constructor() { }
 
