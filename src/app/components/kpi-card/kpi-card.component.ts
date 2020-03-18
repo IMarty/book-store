a0e81f07d8dc63bc9801fb38e2c25bdd85fc35d3
Igor MARTY <igor.marty@telecomnancy.net>
2020-03-18 12:13:34 +0100

    kpi with title and color

--- src/app/components/kpi-card/kpi-card.component.ts
+++ src/app/components/kpi-card/kpi-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kpi-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kpi-card.component.css']
 })
 export class KpiCardComponent implements OnInit {
+  @Input() title;
+  @Input() color;
 
   constructor() { }
 
