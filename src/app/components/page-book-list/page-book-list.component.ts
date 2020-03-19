0c563c6569dfd189bc2e6bd54f9fbc42286378d3
Igor MARTY <igor.marty@telecomnancy.net>
2020-03-19 09:56:02 +0100

    sorting works

--- src/app/components/page-book-list/page-book-list.component.ts
+++ src/app/components/page-book-list/page-book-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from "../../services/book.service";
+import Utils from '../../helper/utils';
+
 @Component({
   selector: 'app-page-book-list',
   templateUrl: './page-book-list.component.html',
@@ -7,7 +9,7 @@ import { BookService } from "../../services/book.service";
 })
 export class PageBookListComponent implements OnInit {
 
-
+  selectedFilter;
   myArray = [];
   constructor(private bookS: BookService) { }
 
@@ -17,6 +19,19 @@ export class PageBookListComponent implements OnInit {
 
   changeAvailability(book) {
     book.available = !book.available;
+  }
+
+  orderName() {
+    this.myArray = (new Utils()).sortString(this.myArray, "name", 1);
+    this.selectedFilter = 'name';
+  }
+  orderAuthor() {
+    this.myArray = (new Utils()).sortString(this.myArray, "author", 1);
+    this.selectedFilter = 'author';
+  }
+  orderPrice() {
+    this.myArray = (new Utils()).sortNumber(this.myArray, "price", 1);
+    this.selectedFilter = 'price';
   }
 }
 // See you at 11h10
